c434eb0f36db56d77d475cb154dfd95346022150
Wanda Montangero <wandamontangero@gmail.com>
2022-05-11 21:58:24 -0300

    avances en seguridad

---
+++ src/main/java/com/huellitas/SeguridadConfiguracion.java
@@ -0,0 +1,52 @@
+
+package com.huellitas;
+
+import com.huellitas.servicios.UsuarioServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled=true)
+public class SeguridadConfiguracion extends WebSecurityConfigurerAdapter {
+
+	@Autowired
+	public UsuarioServicio usuarioServicio;
+	
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth)throws Exception{
+		auth.userDetailsService(usuarioServicio).
+		passwordEncoder(new BCryptPasswordEncoder());
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+
+				.antMatchers("/css/*", "/js/*", "/img/*", "/**","/main/**","/usuario/**","/actividad/**","/login/**", "/glosario/**").permitAll()
+				.and().formLogin()
+					.loginPage("/login")
+						.loginProcessingUrl("/logincheck")
+						.usernameParameter("email")
+						.passwordParameter("password")
+						.defaultSuccessUrl("/")
+						.failureUrl("/login?error=error")
+						.permitAll()
+				.and().logout()
+					.logoutUrl("/logout")
+					.logoutSuccessUrl("/")
+					.permitAll()
+				.and().csrf()
+					.disable();
+	}
+	
+
+}
