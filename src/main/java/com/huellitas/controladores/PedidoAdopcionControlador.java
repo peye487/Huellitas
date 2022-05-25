6c3a87453df5db2876905a8237a336fe6f40cb37
Juan Pablo Ferreyra <peye487@yahoo.com.ar>
2022-05-25 14:02:47 -0300

    Listo adopción y autorización para ingreso a adopción

--- src/main/java/com/huellitas/controladores/PedidoAdopcionControlador.java
+++ src/main/java/com/huellitas/controladores/PedidoAdopcionControlador.java
@@ -2,9 +2,13 @@
 package com.huellitas.controladores;
 
 import com.huellitas.entidades.Mascota;
+import com.huellitas.entidades.Usuario;
 import com.huellitas.servicios.MascotaServicio;
 import com.huellitas.servicios.PedidoAdopcionServicio;
+import com.huellitas.servicios.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.SessionFactoryUtils;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,19 +19,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/pedido-adopcion")
+@PreAuthorize("isAuthenticated()")
 public class PedidoAdopcionControlador {
     @Autowired
     private PedidoAdopcionServicio pAdopcionServicio;
     
     @Autowired
     private MascotaServicio mascotaServicio;
+    
+    @Autowired
+    private UsuarioServicio usuarioServicio;
   
+    
     @GetMapping("/")
-    public String adoptarMascota(@RequestParam String id, ModelMap modelo){
+    public String adoptarMascota(@RequestParam String idMascota, ModelMap modelo){
         
         try {
-            Mascota mascota = mascotaServicio.buscarPorId(id);
-            modelo.addAttribute("mascota", mascota);
+            Mascota mascota = mascotaServicio.buscarPorId(idMascota);
+            modelo.addAttribute("mascota", mascota);            
+            
             
         } catch (Exception e) {
             
